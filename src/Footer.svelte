692c85dbba523a6d36dc517f2f72f61ddb7bf598
Roberto Salas <wala.rsalas@gmail.com>
2021-11-26 22:14:02 -0300

    better sizing

--- src/Footer.svelte
+++ src/Footer.svelte
@@ -5,7 +5,7 @@
 <footer>
   <div class="wrapper">
     <p style="font-size:12px;width: 80%;text-align:center">{text}</p>
-    <div style="height: 125px; width: 80%;">
+    <div style="height: 125px; width: 90%;">
       <img src="images/footer-logos.png" alt="Logos"/>
     </div>
   </div>
