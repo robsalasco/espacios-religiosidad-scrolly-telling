b9224fb0ffece99eac7f06c64966a2e2a7f5d19e
Roberto Salas <wala.rsalas@gmail.com>
2021-12-02 11:37:50 -0300

    other position

--- src/Footer.svelte
+++ src/Footer.svelte
@@ -4,9 +4,9 @@
 
 <footer>
   <div class="wrapper">
-    <p style="font-size:12px;width: 80%;text-align:center">{text}</p>
     <div style="height: 125px; width: 90%;">
       <img src="images/footer-logos.png" alt="Logos"/>
     </div>
+    <p style="font-size:12px;width: 80%;text-align:center;margin-top:40px;margin-bottom:40px">{text}</p>
   </div>
 </footer>
